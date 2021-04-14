350ed51d54cb11b54b5479a6c5cfab60a55bef93
Yazid Rahmouni <yezidrahmouni@gmail.com>
2021-04-14 11:07:13 +0300

    intitial commit

---
+++ src/components/projects.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Link } from "react-router-dom"
+
+class Projects extends Component {
+  state = { activeTab: 0 };
+
+
+  toggleCategories() {
+
+    if (this.state.activeTab === 0) {
+      return (
+        <React.Fragment>
+          <div className="projects-grid">
+            {/* Project 1 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+
+
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://www.w3schools.com/w3css/img_lights.jpg) bottom right 15% no-repeat #46B6AC' }} >React Project #1</CardTitle>
+
+              <CardText>
+                a market place for used furnitures with many cool features.
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a>
+                <a href="https://homeusedfurn-fe.herokuapp.com/" target="_blank">
+                  <Button colored>Live Demo</Button>
+                </a>
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            {/* Project 2 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg) center / cover' }} >React Project #2</CardTitle>
+              <CardText>
+                lightweight Netflix clone with nice user experience!
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz/netflix-clone-updated" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a >
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+            {/* Project 3 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://image.freepik.com/free-photo/double-exposure-image-business-profit-growth_31965-3022.jpg) center / cover' }} >React Project #3</CardTitle>
+              <CardText>
+                whatsApp Clone with nice features!
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz/" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a>
+                <a href="https://whatsapp-front-end.herokuapp.com/" target="_blank">
+                  <Button colored>Live Demo</Button>
+                </a>
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+
+          </div>
+
+          <div className="projects-grid " style={{ marginTop: "2rem" }}>
+            {/* Project 4 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://www.wpexplorer.com/wp-content/uploads/wordpress-image-optimization-guide.jpg) center / cover' }} >React Project 4</CardTitle>
+              <CardText>
+                LinkedIn Clone
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a>
+
+                <Button colored>Live Demo</Button>
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+            {/* Project 5 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg) center / cover' }} >React Project #5</CardTitle>
+              <CardText>
+                Appointment organizer for Doctor.
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a>
+
+
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+            {/* Project 6 */}
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+              <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://wallpaperaccess.com/full/806272.jpg) center / cover' }} >React Project #6</CardTitle>
+              <CardText>
+                web App that provides daily general advices.
+            </CardText>
+              <CardActions border>
+                <a href="https://github.com/Imaginationzz" target="_blank">
+                  <Button colored>GitHub</Button>
+                </a>
+
+
+              </CardActions>
+              <CardMenu style={{ color: '#fff' }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+          </div>
+
+        </React.Fragment>
+
+      )
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div><h1>This is Angular</h1></div>
+      )
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div><h1>This is VueJS</h1></div>
+      )
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div><h1>This is MongoDB</h1></div>
+      )
+    }
+
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+
+
+      </div>
+    )
+  }
+}
+
+export default Projects;
---
+++ src/components/resume.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+
+
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{ textAlign: 'center' }}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{ height: '200px' }}
+              />
+            </div>
+
+            <h2 style={{ paddingTop: '2em' }}>Yazid Rahmouni</h2>
+            <h4 style={{ color: 'grey' }}>Full-Stack Engineer And Financial Consultant</h4>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <p> An International and Multi-Cultural IT and Business Consultant with technical and management skills in different fields ....... As well as multi-cultural skills, having worked in different countries like Tunisia ,The United States of America , India, Qatar. Additionally, through the variety of team projects in university, I am team-oriented with excellent communication ,organization skills and implementing efficient management solutions </p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+            <h5>Address</h5>
+            <p>West Bay Doha Qatar</p>
+            <h5>Phone</h5>
+            <p>(+974) 66203922</p>
+            <h5>Email</h5>
+            <p>yezidrahmouni@gmail.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+
+
+            <Education
+              startYear={2003}
+              endYear={2007}
+              schoolName=" Manar University Tunis "
+              schoolDescription=" Bachelor's Degree in computer science applied to managment"
+            />
+
+            <Education
+              startYear={2012}
+              endYear={2016}
+              schoolName="HBKU Qatar"
+              schoolDescription="Master of Science in Finance"
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+
+            <h2>Experience</h2>
+
+            <Experience
+              startYear={2017}
+              endYear={2021}
+              jobName="Full Stack Engineer And Financial  lecturer @Institute of Public Administration"
+              jobDescription="Giving trainings and lectures in various fields such as : IT , public Finance,Statistics,Project Management.."
+
+
+
+              jobDescription2="designed and developed different Web  applications  using React ,Nodejs,MongoDB Javascript,ECS6…"
+            />
+
+            <Experience
+              startYear={2013}
+              endYear={2017}
+              jobName="IT CONSULTANT — MINISTRY OF ADMINISTRATIVE DEVELOPMENT -QATAR"
+              jobDescription="designed IOS application  "
+              jobDescription2="Management team lead"
+            />
+            <Experience
+              startYear={2010}
+              endYear={2013}
+              jobName="IT CONSULTANT — MINISTRY OF LABOUR -QATAR"
+              jobDescription="INVOLVED IN DESIGN OF WEB APPLICATION , DELIVERED IT TRAININGS  "
+
+              jobDescription2="ADMINISTRATIVE MANAGEMENT"
+            />
+            <Experience
+              startYear={2009}
+              endYear={2010}
+              jobName="QA ENGINEER -USA"
+              jobDescription="Design, implement, execute and debug information technology test cases and scripts • Diagnose, find bugs, defects, and regressions. Verify fixes. "
+
+              jobDescription2="Collaborate at ease with developers, customer support, product managers and operations "
+            />
+            <Experience
+              startYear={2008}
+              endYear={2009}
+              jobName="Internship -India"
+              jobDescription="Internship as a QA ENGINEER  "
+
+              jobDescription2="Besides to some Management tasks "
+            />
+            <hr style={{ borderTop: '3px solid #e22947' }} />
+            <h2>Skills</h2>
+            <Skills
+              skill="javascript"
+              progress={100}
+            />
+            <Skills
+              skill="HTML/CSS"
+              progress={100}
+            />
+            <Skills
+              skill="NodeJS"
+              progress={100}
+            />
+            <Skills
+              skill="React"
+              progress={100}
+            />
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
